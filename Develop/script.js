403178bd19cdd8f9c0922f726a3f54633cbaec28
tanvirpi <tanvir.pi@gmail.com>
2023-01-29 20:39:22 -0700

    text area color changing

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,64 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-const dateElement= document.getElementById('showDate')
+const dateElement = document.getElementById('showDate')
 var now = new Date();
-var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
-var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
-var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+var days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+var date = ((now.getDate() < 10) ? "0" : "") + now.getDate();
 
-function fourdigits(number)  {
-	return (number < 1000) ? number + 1900 : number;
-								}
-  dateElement.innerText =  days[now.getDay()] + ", " +
-         months[now.getMonth()] + " " +
-         date + ", " +
-         (fourdigits(now.getYear())) ;
+function fourdigits(number) {
+  return (number < 1000) ? number + 1900 : number;
+}
+dateElement.innerText = days[now.getDay()] + ", " +
+  months[now.getMonth()] + " " +
+  date + ", " +
+  (fourdigits(now.getYear()));
 
 const timeElement = document.getElementById('showTime');
-      setInterval(function () {
-        const currentDate = new Date();
-        timeElement.innerText = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
-      }, 1000);
+setInterval(function () {
+  const currentDate = new Date();
+  timeElement.innerText = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+}, 1000);
+
+const today = new Date()
+const currentHour = today.getHours()
+console.log(currentHour)
 
 
-const currentHour= 99
 
-const buttonList =  document.querySelectorAll(".btn")
-buttonList.forEach(element => element.addEventListener("click",saveData))
 
-function saveData(){
-  var id =  this.getAttribute("id").split("-")[1]
+const buttonList = document.querySelectorAll(".btn")
+buttonList.forEach(element => element.addEventListener("click", saveData))
 
-  var getData= document.getElementById('textarea-'+id).value
-  console.log(getData,id);
-  localStorage.setItem(id,getData)  //Key / value
+function saveData() {
+  var id = this.getAttribute("id").split("-")[1]
+
+  var getData = document.getElementById('textarea-' + id).value
+  console.log(getData, id);
+  localStorage.setItem(id, getData)  //Key / value
 }
 
-for(let i=9;i<=17;i++)
-{
-  var id = "textarea-"+i
+for (let i = 9; i <= 17; i++) {
+  var id = "textarea-" + i
   var data = localStorage.getItem(i)
-  console.log(id,data)
+  console.log(id, data)
   document.getElementById(id).value = data
+
+  if (i < currentHour) {
+    document.getElementById(id).classList.add('past')
+  } else if (i === currentHour) {
+    document.getElementById(id).classList.add('present')
+  } else {
+    document.getElementById(id).classList.add('future')
+  }
+
 }
+
+
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -49,11 +66,11 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-//   document.getElementById("btn-1").addEventListener("click", function (){
-//     var user= document.getElementById("inputtag").value
-//     localStorage.setItem("quiz", JSON.stringify({user:user,score:(scoreCount+timerCounter)}))
+  //   document.getElementById("btn-1").addEventListener("click", function (){
+  //     var user= document.getElementById("inputtag").value
+  //     localStorage.setItem("quiz", JSON.stringify({user:user,score:(scoreCount+timerCounter)}))
 
-// })
+  // })
 
 
   //
